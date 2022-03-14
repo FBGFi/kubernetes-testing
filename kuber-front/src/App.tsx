27933ba99ee17316ae6e6f26cf0276d9eed6e305
Aleksi Alhola <aleksi.alhola@knowit.fi>
2022-03-14 13:37:47 +0200

    amcharts testing

--- kuber-front/src/App.tsx
+++ kuber-front/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import * as am5 from '@amcharts/amcharts5';
+import * as am5percent from '@amcharts/amcharts5/percent'
 
 import { io } from 'socket.io-client';
 import { SocketClass } from './constants/classes/SocketClient';
@@ -9,15 +11,35 @@ const App = () => {
   const requests = 20;
 
   React.useEffect(() => {
-    if(apiResponse.length < requests){
+    if (apiResponse.length < requests) {
       fetch('http://localhost:3000/')
         .then(res => res.json())
         .then(data => setApiResponse(apiResponse.concat([data.data])))
     }
-  }, [apiResponse])
+  }, [apiResponse]);
+
+  React.useEffect(() => {
+    const root = am5.Root.new("chartdiv");
+    const chart = root.container.children.push(am5percent.PieChart.new(root, {}));
+    const series = chart.series.push(am5percent.PieSeries.new(root, {
+      valueField: "value",
+      categoryField: "category"
+    }));
+    series.data.setAll([{
+      category: "Research",
+      value: 1000
+    }, {
+      category: "Marketing",
+      value: 1200
+    }, {
+      category: "Sales",
+      value: 850
+    }]);
+  })
 
   return (
     <div className="App">
+      <div id="chartdiv"></div>
       {apiResponse.map(res => <p>{res}</p>)}
     </div>
   );
